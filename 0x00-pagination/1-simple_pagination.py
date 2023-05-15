ca4a9e0a88c5837ce9c8ddbbc554d3e1c7816cc5
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-05-15 19:08:02 +0100

    get_page

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,54 +1,63 @@
 #!/usr/bin/env python3
-"""
-Write a function named index_range that takes two integer arguments
-page and page_size.
+"""Write a function named index_range that takes two integer
+arguments page and page_size.
 
-The function should return a tuple of size two containing a start index
-and an end index corresponding to the range of indexes to return in a
-list for those particular pagination parameters.
+The function should return a tuple of size two containing a
+start index and an end index corresponding to the range of
+indexes to return in a list for those particular pagination
+parameters.
 
 Page numbers are 1-indexed, i.e. the first page is page 1.
 """
 
+
 from typing import Tuple, List
 import csv
+import math
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Returns a tuple of start and end indices for a given page and page_size.
+    start index and an end index corresponding to the range of
     """
-    start = (page - 1) * page_size
-    end = start + page_size
-    return start, end
+    # if page is 1, start at 0 and end at page_size
+    # if page is 2, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    # if page is 3, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    return ((page-1) * page_size, page_size * page)
 
 
 class Server:
-    """
-    Server class to paginate a database of popular baby names.
+    """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        self._dataset = None
+        self.__dataset = None
 
-    def dataset(self) -> List[List[str]]:
+    def dataset(self) -> List[List]:
+        """Cached dataset
         """
-        Returns the cached dataset
-        """
-        if self._dataset is None:
+        if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
-                self._dataset = [row for row in reader][1:]
-        return self._dataset
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List[str]]:
-        """
-        Returns the appropriate page of the dataset
-        """
-        assert isinstance(page, int) and page > 0, "Invalid page number"
-        assert isinstance(page_size, int) and page_size > 0, "Invalid page size"
+        return self.__dataset
 
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return the appropriate page of the dataset"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        # get the data from the csv
         data = self.dataset()
-        start, end = index_range(page, page_size)
-        return data[start:end]
+
+        try:
+            # get the index to start and end at
+            start, end = index_range(page, page_size)
+            return data[start:end]
+        except IndexError:
+            return []
