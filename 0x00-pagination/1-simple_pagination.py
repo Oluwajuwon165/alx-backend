1f2704e381cd618db0279985c5f273785d194240
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-05-15 19:06:42 +0100

    get_page

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,62 +1,54 @@
 #!/usr/bin/env python3
 """
-Module containse a server that paginates a database of popular baby names
+Write a function named index_range that takes two integer arguments
+page and page_size.
+
+The function should return a tuple of size two containing a start index
+and an end index corresponding to the range of indexes to return in a
+list for those particular pagination parameters.
+
+Page numbers are 1-indexed, i.e. the first page is page 1.
 """
+
+from typing import Tuple, List
 import csv
-import math
-from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Args:
-        page (int): page number to return (pages are 1-indexed)
-        page_size (int): number of items per page
-    Return:
-        tuple(start_index, end_index)
+    Returns a tuple of start and end indices for a given page and page_size.
     """
-    start, end = 0, 0
-    for i in range(page):
-        start = end
-        end += page_size
-
-    return (start, end)
+    start = (page - 1) * page_size
+    end = start + page_size
+    return start, end
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """
+    Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        self.__dataset = None
+        self._dataset = None
 
-    def dataset(self) -> List[List]:
-        """Cached dataset
+    def dataset(self) -> List[List[str]]:
         """
-        if self.__dataset is None:
+        Returns the cached dataset
+        """
+        if self._dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+                self._dataset = [row for row in reader][1:]
+        return self._dataset
 
-        return self.__dataset
-
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List[str]]:
         """
-        Args:
-            page (int): required page number. must be a positive integer
-            page_size (int): number of records per page. must be a +ve integer
-        Return:
-            list of lists containing required data from the dataset
+        Returns the appropriate page of the dataset
         """
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
+        assert isinstance(page, int) and page > 0, "Invalid page number"
+        assert isinstance(page_size, int) and page_size > 0, "Invalid page size"
 
-        dataset = self.dataset()
-        data_length = len(dataset)
-        try:
-            index = index_range(page, page_size)
-            return dataset[index[0]:index[1]]
-        except IndexError:
-            return []
+        data = self.dataset()
+        start, end = index_range(page, page_size)
+        return data[start:end]
